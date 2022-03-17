192601a9a6c47adbfa69011708cb83ca65764042
zaiiin-u <zainarshad.17@hotmail.com>
2022-03-17 20:02:41 +0500

    add navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
